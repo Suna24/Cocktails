b88df1e96035717dbd943cc961d3028810a9873b
Eldras <simon72610@gmail.com>
2022-03-23 23:40:32 -0400

    #6 Vérification de la réaction de l'utilisateur

--- cocktailweb_project/Model/BDDToLikeSystem.php
+++ cocktailweb_project/Model/BDDToLikeSystem.php
@@ -1,19 +1,64 @@
 <?php
 
-include_once ('db.inc.php');
+include_once('db.inc.php');
 
-    $cocktailName = $_POST['name'];
+$cocktailName = $_POST['name'];
 
-    //Création de la base de données
-    $db = new PDO("$server:host=$host;dbname=$base", $user, $pass);
-    $sql="";
+//Création de la base de données
+$db = new PDO("$server:host=$host;dbname=$base", $user, $pass);
+$sql = "";
 
-    if(isset($_POST['like'])) {
-        $sql = "UPDATE cocktail SET `like` = `like` + 1 WHERE cname = '{$cocktailName}';";
-    } elseif (isset($_POST['dislike'])){
-        $sql = "UPDATE cocktail SET `dislike` = `dislike` + 1 WHERE cname = '{$cocktailName}';";
+if (isset($_POST['like'])) {
+    // TODO: Vérifier si l'utiliateur a déjà liké le cocktail
+    $sql = "UPDATE cocktail SET `like` = `like` + 1 WHERE cname = '{$cocktailName}';";
+} elseif (isset($_POST['dislike'])) {
+    $sql = "UPDATE cocktail SET `dislike` = `dislike` + 1 WHERE cname = '{$cocktailName}';";
+}
+$db->exec($sql);
+
+function checkUserReaction($cocktailName)
+{
+    $reaction = getUserReaction(getCocktailId($cocktailName));
+    if ($reaction != "like" && $reaction != "dislike") {
+        return false;
     } else {
-        $sql="";
+        return true;
     }
-    $db->exec($sql);
+}
 
+function getCocktailId($cocktailName)
+{
+    $db = new PDO("$server:host=$host;dbname=$base", $user, $pass);
+    $sql = "SELECT `id` FROM cocktail WHERE cname = '{$cocktailName}';";
+    $result = $db->query($sql);
+    $result = $result->fetch();
+    foreach ($result as $key => $value) {
+        $cocktailId = $value;
+        return $cocktailId;
+    }
+}
+
+function getUserId($userName)
+{
+    $db = new PDO("$server:host=$host;dbname=$base", $user, $pass);
+    $sql = "SELECT `id` FROM user WHERE username = '{$userName}';";
+    $result = $db->query($sql);
+    $result = $result->fetch();
+    foreach ($result as $key => $value) {
+        $id = $value;
+    }
+    return $id;
+}
+
+function getUserReaction($cocktailId)
+{
+    $userId = getUserId($_SESSION['username']);
+    $sql = "SELECT `reaction` FROM reactions WHERE id = '{$cocktailId}' and user_id = '{$userId}';";
+    foreach ($db->query($sql) as $row) {
+        if ($row['reaction'] == 1) {
+            return "like";
+        } elseif ($row['reaction'] == -1) {
+            return "dislike";
+        }
+    }
+}
--- cocktailweb_project/View/ajout.php
+++ cocktailweb_project/View/ajout.php
@@ -1,9 +1,11 @@
 <?php
-include_once "../Model/Login_System/admin_check.php"
+include_once "../Model/Login_System/admin_check.php";
 ?>
-<!DOCTYPE html><html lang="fr">
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-    <link rel="icon" href="https://cdn.discordapp.com/attachments/927911630602403880/931129861945294849/cocococococococockail.png">
+    <link rel="icon"
+          href="https://cdn.discordapp.com/attachments/927911630602403880/931129861945294849/cocococococococockail.png">
     <meta charset="UTF-8">
     <title>TurboCocktail - Ajout</title>
 
@@ -11,59 +13,61 @@ include_once "../Model/Login_System/admin_check.php"
     <link href="CSS_STYLES/accueil.css" rel="stylesheet">
 </head>
 <body>
-    <!-- Barre de navigation -->
-    <nav role="navigation" id="nav">
-        <button id="openbtn_id" class="openbtn" onclick="window.location.href='accueil.html'">Accueil</button>
-        <p id="title">Ajouter un cocktail à la base de données</p>
-    </nav>
+<!-- Barre de navigation -->
+<nav role="navigation" id="nav">
+    <button id="openbtn_id" class="openbtn" onclick="window.location.href='accueil.html'">Accueil</button>
+    <p id="title">Ajouter un cocktail à la base de données</p>
+</nav>
 
-    <!-- Barre de progression -->
-    <div id="progress"></div>
+<!-- Barre de progression -->
+<div id="progress"></div>
 
-    <div class="page">
-        <form id="ajouter" method="post" action="../Model/ajout.php">
+<div class="page">
+    <form id="ajouter" method="post" action="../Model/ajout.php">
 
-            <fieldset>
-                <h3>Le cocktail</h3>
-                <input class="cocktailName" id="cocktailName" type="text" placeholder="Le nom de votre cocktail" pattern="[^()/><\][\\\x22,;|]+" required/>
-            </fieldset>
+        <fieldset>
+            <h3>Le cocktail</h3>
+            <input class="cocktailName" id="cocktailName" type="text" placeholder="Le nom de votre cocktail"
+                   pattern="[^()/><\][\\\x22,;|]+" required/>
+        </fieldset>
 
-            <fieldset id="ing">
-                <h3>Les ingrédients</h3>
-                <div id="firstNode" class="ingredient">
-                    <input id="ingredient" class="nameIngredient" list="ingredients" type="text" placeholder="Ingrédient" required/>
-                    <datalist id="ingredients"></datalist>
-                    <input class="quantite" type=text" placeholder="Quantité" required/>
-                    <input class="unite" type="text" placeholder="Unité" required/>
-                </div>
-
-                <span onclick="newElement()" class="add">+</span>
-            </fieldset>
-
-            <fieldset>
-                <h3>La recette</h3>
-                <textarea id="recette" type="text" placeholder="La recette" required></textarea>
-            </fieldset>
-
-            <fieldset>
-                <h3>La photo</h3>
-                <input id="photo" type="text" placeholder="Lien de la photo" required/>
-            </fieldset>
-
-            <input name="payload" type="hidden" id="payload"/>
-
-            <div id="containAddLink">
-                <input id="addCocktail" class="ajout" type="submit" value="Enregistrer le cocktail !"/>
+        <fieldset id="ing">
+            <h3>Les ingrédients</h3>
+            <div id="firstNode" class="ingredient">
+                <input id="ingredient" class="nameIngredient" list="ingredients" type="text" placeholder="Ingrédient"
+                       required/>
+                <datalist id="ingredients"></datalist>
+                <input class="quantite" type=text" placeholder="Quantité" required/>
+                <input class="unite" type="text" placeholder="Unité" required/>
             </div>
 
-       </form>
-    </div>
+            <span onclick="newElement()" class="add">+</span>
+        </fieldset>
 
-    <!-- Bas de page -->
-    <footer id="footer">
-        <p class="firstFooter">Réalisé par le turbo groupe : Antonin, Bastien, Erhan, Simon</p>
-        <p>Mentions légales, All Rights Reserved <a href="login-page.php">©</a></p>
-    </footer>
+        <fieldset>
+            <h3>La recette</h3>
+            <textarea id="recette" type="text" placeholder="La recette" required></textarea>
+        </fieldset>
+
+        <fieldset>
+            <h3>La photo</h3>
+            <input id="photo" type="text" placeholder="Lien de la photo" required/>
+        </fieldset>
+
+        <input name="payload" type="hidden" id="payload"/>
+
+        <div id="containAddLink">
+            <input id="addCocktail" class="ajout" type="submit" value="Enregistrer le cocktail !"/>
+        </div>
+
+    </form>
+</div>
+
+<!-- Bas de page -->
+<footer id="footer">
+    <p class="firstFooter">Réalisé par le turbo groupe : Antonin, Bastien, Erhan, Simon</p>
+    <p>Mentions légales, All Rights Reserved</p>
+</footer>
 
 </body>
 <script src="../Controller/loadbar.js"></script>
